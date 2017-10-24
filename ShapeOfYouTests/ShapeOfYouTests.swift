7fc583f63c4ee29d7d8fd8496ea5cf207c149bd4
andreiuta <andreiuta.si@gmail.com>
2017-10-24 16:51:33 +0300

    Initial Commit

---
+++ ShapeOfYouTests/ShapeOfYouTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShapeOfYouTests.swift
+//  ShapeOfYouTests
+//
+//  Created by Andreea Stegerean on 24/10/2017.
+//  Copyright © 2017 Andreea Stegerean. All rights reserved.
+//
+
+import XCTest
+@testable import ShapeOfYou
+
+class ShapeOfYouTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
