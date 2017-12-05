2e51bd63fc02748a0fdd9d0cd8f947a963393744
andreiuta <andreiuta.si@gmail.com>
2017-12-05 14:04:34 +0200

    Add a food+ Delete a food

---
+++ ShapeOfYou/ViewControllers/AddFoodViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddFoodViewController.swift
+//  ShapeOfYou
+//
+//  Created by Andreea Stegerean on 29/11/2017.
+//  Copyright © 2017 Andreea Stegerean. All rights reserved.
+//
+
+import UIKit
+
+class AddFoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShapeOfYou/ViewModels/CreateFoodViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CreateFoodViewModel.swift
+//  ShapeOfYou
+//
+//  Created by Andreea Stegerean on 29/11/2017.
+//  Copyright © 2017 Andreea Stegerean. All rights reserved.
+//
+
+import Foundation
